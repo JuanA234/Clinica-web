e9cf866d48b99cd88683ac7ef88f5b0726f42a9a
Juan Avendaño <juanandreslujan30@gmail.com>
2025-04-19 14:58:45 -0500

    pruebas de integracion para ConsultRoomRepository

--- src/main/java/edu/unimagdalena/clinica/repository/ConsultRoomRepository.java
+++ src/main/java/edu/unimagdalena/clinica/repository/ConsultRoomRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface ConsultRoomRepository extends JpaRepository<ConsultRoom, Long> {
     Optional<ConsultRoom> findById(Long id);
     boolean existsById(Long id);
+
 }
