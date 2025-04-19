d2bf536e3ab1b1e2114bb4517e7f6d4819b15b32
Juan Avendaño <juanandreslujan30@gmail.com>
2025-04-19 10:57:40 -0500

    Prueba de integracion para el repositorio de paciente

---
+++ src/test/java/edu/unimagdalena/clinica/repository/PatientRepositoryTest.java
@@ -0,0 +1,48 @@
+package edu.unimagdalena.clinica.repository;
+
+import edu.unimagdalena.clinica.entity.Patient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Import(TestcontainersConfiguration.class)
+@Testcontainers
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PatientRepositoryTest {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    Patient patient;
+
+
+    @BeforeEach
+    void setUp() {
+        patient = new Patient(null, "Juan Avendaño", "juanavenda@gmail.com", null, null);
+        patientRepository.save(patient);
+    }
+
+    @Test
+    void findById() {
+        Optional<Patient> found = patientRepository.findById(patient.getId());
+
+        assertTrue(found.isPresent());
+        assertEquals(patient.getId(), found.get().getId());
+        assertEquals(patient.getFullName(), found.get().getFullName());
+    }
+
+    @Test
+    void existsById() {
+        assertTrue(patientRepository.existsById(patient.getId()));
+    }
+}
