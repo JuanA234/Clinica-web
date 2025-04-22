97dee5823f18bea5036a2f14e90b3ed1816f1060
Juan Avendaño <juanandreslujan30@gmail.com>
2025-04-22 13:15:02 -0500

    Agregado excepciones

---
+++ src/main/java/edu/unimagdalena/clinica/exception/ApiError.java
@@ -0,0 +1,15 @@
+package edu.unimagdalena.clinica.exception;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Builder
+public record ApiError(
+        LocalDateTime timestamp,
+        Integer status,
+        String message,
+        Map<String, String> errors// Usado para errores de validación, puede ser null
+) {
+}
