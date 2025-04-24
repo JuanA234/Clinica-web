27151cd11fb4e2787930d164385f74262a6d0fbd
Juan Sebastián Sarmiento <sarmientun@gmail.com>
2025-04-24 14:19:05 -0500

    AppointmentController creado

--- src/main/java/edu/unimagdalena/clinica/controller/AppointmentController.java
+++ src/main/java/edu/unimagdalena/clinica/controller/AppointmentController.java
@@ -1,4 +1,56 @@
 package edu.unimagdalena.clinica.controller;
 
+import edu.unimagdalena.clinica.dto.Appointment.CreateAppointmentDTO;
+import edu.unimagdalena.clinica.dto.Appointment.ResponseAppointmentDTO;
+import edu.unimagdalena.clinica.dto.Appointment.UpdateAppointmentDTO;
+import edu.unimagdalena.clinica.service.interfaces.AppointmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/appointments")
 public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponseAppointmentDTO>> getAllAppointments() {
+        return ResponseEntity.ok(appointmentService.findAllAppointments());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseAppointmentDTO> getAppointmentById(@PathVariable Long id) {
+        return ResponseEntity.ok(appointmentService.findAppointmentById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseAppointmentDTO> createAppointment(@RequestBody CreateAppointmentDTO request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(appointmentService.createAppointment(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseAppointmentDTO> updateAppointment(@PathVariable Long id, @RequestBody UpdateAppointmentDTO request) {
+        return ResponseEntity.ok(appointmentService.updateAppointment(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAppointment(@PathVariable Long id) {
+        appointmentService.deleteAppointment(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(params = {"doctorId", "date"})
+    public ResponseEntity<List<ResponseAppointmentDTO>> getAppointmentByDoctorId(@RequestParam Long doctorId, @RequestParam LocalDate date){
+        return ResponseEntity.ok(appointmentService.findAppointmentByDoctorId(doctorId, date));
+    }
+
 }
+
